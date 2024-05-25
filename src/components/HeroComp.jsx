f1c8468112441fb65e3f80300d6c66b4ae18af2e
= <abhisingh30800@gmail.com>
2024-05-25 12:52:04 +0530

    made separate hero comp for different routes

---
+++ src/components/HeroComp.jsx
@@ -0,0 +1,20 @@
+const HeroComp = ({ img, heading, subHeading }) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${img})` }}
+      className="flex flex-col gap-5 justify-center items-start h-[60vh] md:h-[70vh]  bg-cover bg-center px-6 md:px-12"
+    >
+      <h1 className="text-3xl md:text-4xl mt-7 lg:text-[3rem] xl:text-[3.5rem] font-bold">
+        {heading}
+      </h1>
+      <p className="md:text-lg lg:text-xl xl:mt-3 xl:text-2xl font-medium">
+        {subHeading}
+      </p>
+      <button className="bg-white py-2 px-4 md:py-3 md:px-6 rounded-sm uppercase shadow-sm mt-8 text-sky-900 lg:text-lg font-semibold transition duration-200 linear hover:bg-sky-500 hover:text-white">
+        Contact Us
+      </button>
+    </div>
+  );
+};
+
+export default HeroComp;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,21 +1,9 @@
-// import subHeroImg from "./src/assets/subHeroImg1.png";
-const HeroSection = ({ img, heading, subHeading }) => {
+import HeroComp from "./HeroComp";
+
+const HeroSection = (props) => {
   return (
     <section>
-      <div
-        style={{ backgroundImage: `url(${img})` }}
-        className="flex flex-col gap-5 justify-center items-start h-[60vh] md:h-[70vh]  bg-cover bg-center px-6 md:px-12"
-      >
-        <h1 className="text-3xl md:text-4xl mt-7 lg:text-[3rem] xl:text-[3.5rem] font-bold">
-          {heading}
-        </h1>
-        <p className="md:text-lg lg:text-xl xl:mt-3 xl:text-2xl font-medium">
-          {subHeading}
-        </p>
-        <button className="bg-white py-2 px-4 md:py-3 md:px-6 rounded-sm uppercase shadow-sm mt-8 text-sky-900 lg:text-lg font-semibold transition duration-200 linear hover:bg-sky-500 hover:text-white">
-          Contact Us
-        </button>
-      </div>
+      <HeroComp {...props} />
       {/* Sub Hero Section */}
       <div className="flex flex-col md:flex-row gap-10 md:gap-20 text-center md:text-left p-16 md:p-24 xl:p-36 items-center justify-center">
         <div className="flex flex-col gap-10 md:basis-1/2">
