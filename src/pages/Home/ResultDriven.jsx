f6c745cf8e87145c3a2518f09a29b22cd91ff8fd
= <abhisingh30800@gmail.com>
2024-05-26 21:08:41 +0530

    changed the padding of benefit wrapper component

--- src/pages/Home/ResultDriven.jsx
+++ src/pages/Home/ResultDriven.jsx
@@ -3,7 +3,7 @@ import BenefitsWrapper from "./BenefitsWrapper";
 
 export default function ResultDriven() {
   return (
-    <section className="py-10 px-5 md:p-20">
+    <section className="py-10 px-4 md:p-12 lg:p-20">
       <SectionTitle
         title="result driven"
         subText="Web Apps Offer A Range Of Benefits For Business For Business Across Various Industries"
