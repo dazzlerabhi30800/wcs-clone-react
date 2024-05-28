f89a7bf6fcec9af964a825c4aa9bbb21e6b9d53f
= <abhisingh30800@gmail.com>
2024-05-28 09:05:43 +0530

    added navbar active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
   const location = useLocation();
   useEffect(() => {
     setShowLinks(false);
+    window.scrollTo(0, 0);
   }, [location.pathname]);
   return (
     <nav className="flex flex-col md:flex-row gap-4 md:gap-0 md:justify-between  bg-black/50 border-2 border-indigo-400 md:items-center fixed top-4 w-[96%] py-[10px] px-4 md:px-6 rounded-lg -translate-x-[50%] left-1/2 backdrop-blur-2xl">
@@ -32,22 +33,76 @@ const Navbar = () => {
         }  gap-6 md:gap-10 items-center transition duration-300 ease-in-out text-base md:text-lg py-3 md:py-0`}
       >
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            className={`py-1 border-b ${
+              location.pathname === "/"
+                ? "font-bold text-cyan-400 border-sky-500"
+                : "border-transparent font-normal"
+            }`}
+            to="/"
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/services">Services</Link>
+          <Link
+            className={`py-1 border-b ${
+              location.pathname === "/services"
+                ? "font-bold text-cyan-400 border-sky-500"
+                : "border-transparent font-normal"
+            }`}
+            to="/services"
+          >
+            Services
+          </Link>
         </li>
         <li>
-          <Link to="/portfolio">Portfolio</Link>
+          <Link
+            className={`py-1 border-b ${
+              location.pathname === "/portfolio"
+                ? "font-bold text-cyan-400 border-sky-500"
+                : "border-transparent font-normal"
+            }`}
+            to="/portfolio"
+          >
+            Portfolio
+          </Link>
         </li>
         <li>
-          <Link to="/blog">Blog</Link>
+          <Link
+            className={`py-1 border-b ${
+              location.pathname === "/blog"
+                ? "font-bold text-cyan-400 border-sky-500"
+                : "border-transparent font-normal"
+            }`}
+            to="/blog"
+          >
+            Blog
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link
+            className={`py-1 border-b ${
+              location.pathname === "/about"
+                ? "font-bold text-cyan-400 border-sky-500"
+                : "border-transparent font-normal"
+            }`}
+            to="/about"
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link
+            className={`py-1 border-b ${
+              location.pathname === "/contact"
+                ? "font-bold text-cyan-400 border-sky-500"
+                : "border-transparent font-normal"
+            }`}
+            to="/contact"
+          >
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
